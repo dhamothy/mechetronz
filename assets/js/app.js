828135fed52ace6c9c26c9cba7993b6b69f5d191
cb-dtn <dtn@chargebee.com>
2017-06-23 12:46:21 +0530

    menu and js change

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,10 @@
-// $(document).ready(function(){
-
-// 	$(window).scroll(function() {
-// 	    var scroll = $(window).scrollTop();
-// 	    if (scroll >= 1) {
-// 	        $(".me-header").addClass("me-header--affix");
-// 	    } else {
-// 	    	$(".me-header--affix").removeClass("me-header--affix");
-// 	    }
-// 	});
-// });
+$(document).ready(function(){
+	$(window).scroll(function() {
+	    var scroll = $(window).scrollTop();
+	    if (scroll >= 1) {
+	        $(".me-header").addClass("me-header--affix");
+	    } else {
+	    	$(".me-header--affix").removeClass("me-header--affix");
+	    }
+	});
+});
