3f3ab327b2d380653596c1133fdf40661b74307b
cb-dtn <dtn@chargebee.com>
2017-07-30 14:21:25 +0530

    site responsive to devices

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,4 +7,17 @@ $(document).ready(function(){
 	    	$(".me-header--affix").removeClass("me-header--affix");
 	    }
 	});
+
+	$('a[href*=#]:not([href=#])').click(function() {
+	    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+	      	var target = $(this.hash);
+	      	target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+	      	if (target.length) {
+	        	$('html,body').animate({
+	          		scrollTop: target.offset().top - 54
+	        	}, 500);
+	        	return false;
+	      	}
+	    }
+	});
 });
